3d25bfadd9da84530c5ea04c87d9005e781f9b39
HipsterSloth <hipstersloth908@gmail.com>
2017-05-07 21:20:40 -0700

    Build fixes * Adjusted Qt paths in client_overlay.vcxproj from "..\..\..\..\Development\Qt\Qt5.7.0\5.7\msvc2015_64" to $(QTDIR)  ** Set QTDIR in client_overlay.vcxproj.user * Renamed usages of "vrmath::quatFromRotationYXZ" -> "vrmath::quaternionFromYawPitchRoll(0.0f, -0.1f, 0.0f);" * Renamed usage of "vrmath::quatFromRotMat" -> "vrmath::quaternionFromRotationMatrix"

--- client_leapmotion/src/LeapMotionClient.h
+++ client_leapmotion/src/LeapMotionClient.h
@@ -16,14 +16,14 @@ private:
 	vr::DriverPose_t leftPose;
 	vr::VRControllerState_t leftState;
 	double leftTranslationOffset[3] = {0.0, 0.0, -0.15};
-	vr::HmdQuaternion_t leftRotationOffset = vrmath::quatFromRotationYXZ(0.0f, -0.1f, 0.0f);
+	vr::HmdQuaternion_t leftRotationOffset = vrmath::quaternionFromYawPitchRoll(0.0f, -0.1f, 0.0f);
 
 	uint32_t virtualIdRight;
 	bool rightReadyFlag = false;
 	vr::DriverPose_t rightPose;
 	vr::VRControllerState_t rightState;
 	double rightTranslationOffset[3] = { 0.0, 0.0, -0.15 };
-	vr::HmdQuaternion_t rightRotationOffset = vrmath::quatFromRotationYXZ(0.0f, -0.1f, 0.0f);
+	vr::HmdQuaternion_t rightRotationOffset = vrmath::quaternionFromYawPitchRoll(0.0f, -0.1f, 0.0f);
 
 public:
 	LeapMotionClient(Leap::Controller* leapController, vrinputemulator::VRInputEmulator* inputEmulator);
